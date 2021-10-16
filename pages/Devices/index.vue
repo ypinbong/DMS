a15abdd466c783f4cb8e505d34653913aaf16c61
Yong Pin <yongpinbong@gmail.com>
2021-10-16 11:05:52 +0800

    added v-autocomplete code

--- pages/Devices/index.vue
+++ pages/Devices/index.vue
@@ -1,10 +1,54 @@
 <template>
-  <h1>devices</h1>
+  <v-card>
+    <h1>devices</h1>
+    <v-container fluid>
+      <v-row align="center">
+        <v-col cols="12">
+          <v-autocomplete
+            v-model="values"
+            :items="items"
+            outlined
+            dense
+            chips
+            small-chips
+            label="Outlined"
+            multiple
+          ></v-autocomplete>
+        </v-col>
+        <v-col cols="12">
+          <v-autocomplete
+            v-model="values"
+            :items="items"
+            dense
+            chips
+            small-chips
+            label="Solo"
+            multiple
+            solo
+          ></v-autocomplete>
+        </v-col>
+        <v-col cols="12">
+          <v-autocomplete
+            v-model="value"
+            :items="items"
+            dense
+            filled
+            label="Filled"
+          ></v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
 export default {
-  layout: 'sidebar-layout'
+  layout: 'sidebar-layout',
+  data: () => ({
+    items: ['foo', 'bar', 'fizz', 'buzz'],
+    values: ['foo', 'bar'],
+    value: null,
+  }),
 }
 </script>
 
